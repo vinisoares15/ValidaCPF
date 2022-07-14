ee18989e18b57ba9090225870af3e71cbe38a824
Vinícius Soares de Figueiredo <vsoares@algartelecom.com.br>
2022-07-13 21:17:52 -0300

    retirar a parte inutilizada de RG

--- ValidaCPF/geraCpfeCnpj/GeraCpfCnpj.java
+++ ValidaCPF/geraCpfeCnpj/GeraCpfCnpj.java
@@ -3,10 +3,6 @@ import java.util.InputMismatchException;
 import java.util.Random;
 public class GeraCpfCnpj {
 
-    private int randomiza(int n) {
-        int ranNum = (int) (Math.random() * n);
-        return ranNum;
-    }
 
     private int mod(int dividendo, int divisor) {
         return (int) Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
@@ -50,44 +46,12 @@ public class GeraCpfCnpj {
     }
 
 
-    public String rg(boolean comPontos) {
-        String  nDigResult;
-        String numerosContatenados;
-        String numeroGerado;
-        Random numeroAleatorio = new Random();
-        //numeros gerados
-        int n1 = numeroAleatorio.nextInt(10);
-        int n2 = numeroAleatorio.nextInt(10);
-        int n3 = numeroAleatorio.nextInt(10);
-        int n4 = numeroAleatorio.nextInt(10);
-        int n5 = numeroAleatorio.nextInt(10);
-        int n6 = numeroAleatorio.nextInt(10);
-        int n7 = numeroAleatorio.nextInt(10);
-        int n8 = numeroAleatorio.nextInt(10);
-        int n9 = numeroAleatorio.nextInt(10);
-
-        //Conctenando os numeros
-        numerosContatenados = String.valueOf(n1) + String.valueOf(n2) + String.valueOf(n3)  + String.valueOf(n4) +
-                String.valueOf(n5) + String.valueOf(n6) + String.valueOf(n7) +String.valueOf(n8)  +
-                String.valueOf(n9);
-        numeroGerado = numerosContatenados;
-
-        if (comPontos)
-            numeroGerado = "" + n1 + n2 + "." + n3 + n4 + n5 + "." + n6 + n7 + n8 + "-" + n9;
-        else
-            numeroGerado = "" + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9;
-
-        return numeroGerado;
-    }
-
 
     public static void main(String[] args) {
         GeraCpfCnpj gerador = new GeraCpfCnpj();
         String cpf = gerador.cpf(true);
         System.out.printf("CPF: %s, Valido: %s\n", cpf, gerador.isCPF(cpf));
 
-        String rg = gerador.rg(true);
-        System.out.printf("RG: %s", rg);
     }
 
 
@@ -161,8 +125,4 @@ public class GeraCpfCnpj {
         return doc;
     }
 
-    public static String imprimeCNPJ(String CNPJ) {
-        // máscara do CNPJ: 99.999.999.9999-99
-        return (CNPJ.substring(0, 2) + "." + CNPJ.substring(2, 5) + "." + CNPJ.substring(5, 8) + "." + CNPJ.substring(8, 12) + "-" + CNPJ.substring(12, 14));
-    }
 }
